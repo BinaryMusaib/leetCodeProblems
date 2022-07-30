cd7de6a6a2e0ecb5667eb2f4f3482e021460a257
BinaryMusaib <hotmusaib@gmail.com>
2022-07-30 14:51:32 +0530

    Palindrome Number program

---
+++ easyProblems/pallindromeNumber.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int temp = x, reverse = 0;
+        while (temp > 0) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return x == reverse;
+        
+    }
+}
